968c14f09937ca3e3f5ef4e6628a5e8771c9368d
Ikone <elmaildebenja@gmail.com>
2018-06-20 23:47:17 -0700

    Define view for button and editText

--- app/src/main/java/com/example/ben/iss/MainActivity.java
+++ app/src/main/java/com/example/ben/iss/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.ben.iss;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public
 class MainActivity extends AppCompatActivity {
+
+    EditText myEditText;
+    Button myButton;
 
     @Override
     protected
     void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         myEditText = findViewById(R.id.tipValue);
+         myButton = findViewById(R.id.getValueButton);
     }
 }
